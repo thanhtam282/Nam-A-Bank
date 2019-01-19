a516f6d877a11d4ce95fe1403aab81db03f04689
Pisces <>
2019-01-19 22:18:47 +0700

    update

--- _components/index/i-7/index.js
+++ _components/index/i-7/index.js
@@ -7,15 +7,20 @@ function block_7swiper() {
             nextEl: '.block-index-7 .swiper-button-next',
             prevEl: '.block-index-7 .swiper-button-prev',
         },
-        autoplay: {
-            disableOnInteraction: false,
-            delay: 6000,
-        },
+        // autoplay: {
+        //     disableOnInteraction: false,
+        //     delay: 6000,
+        // },
         breakpoints: {
+            320: {
+                effect: "fade",
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
             420: {
                 effect: "fade",
                 slidesPerView: 1,
-                spaceBetween: 30
+                spaceBetween: 20
             },
             550: {
                 effect: "fade",
@@ -35,24 +40,61 @@ function block_7swiper() {
         },
     })
 }
+
 block_7swiper();
 
-
+function block_7_slick() {
+    if ($('.block-index-7 .list-items').length) {
+        $(".block-index-7 .list-items").slick({
+            autoplay: false,
+			// slickPlay: true,
+			// slickPause: true,
+			autoplaySpeed: 4000,
+			dots: false,
+			infinite: true,
+			speed: 300,
+			arrows: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			// customPaging: function(slider, i) {
+                //     var thumb = $(slider.$slides[i]).data('thumb');
+                //     return '<a><p> ' + thumb + '</p></a>';
+                // },
+                responsive: [{
+                    breakpoint: 480,
+					settings: {
+                        dots: false,
+					}
+				}
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			]
+		});
+	}
+    
+};
+// block_7_slick()
 
 function setHeightH3() {
-    var maxHeight = 0;
+    var stHeight = $('.block-index-7 .swiper-container ').height();
+    $('.block-index-7 .swiper-slide').css('height',stHeight + 'px' );
+    // var maxHeight = 0;
+    
+    // $('.block-index-7 .swiper-slide ').each(function () {
+    //     var thisH = $(this).height();
+    //     if (thisH > maxHeight) {
+    //         maxHeight = thisH;
+    //     }
 
-    $('.block-index-7 .swiper-slide ').each(function () {
-        var thisH = $(this).height();
-        if (thisH > maxHeight) {
-            maxHeight = thisH;
-        }
-
-    });
-    $('.block-index-7 .swiper-slide ').height(maxHeight)
+    // });
+    // $('.block-index-7 .swiper-slide ').height(maxHeight)
 }
 $(function () {
-    setHeightH3()
+    setTimeout(() => {
+        
+        setHeightH3()
+    }, 500);
 
 })
 
